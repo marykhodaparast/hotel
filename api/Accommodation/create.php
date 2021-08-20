e06c2c6ea0dd5a754a57902b4c1f0cd149f178af
maryam khodaparast <khcomputergirl@gmail.com>
2021-08-21 01:14:32 +0430

    accommodation create but not tested completely

---
+++ api/Accommodation/create.php
@@ -0,0 +1,82 @@
+<?php
+require '../../config/Database.php';
+require '../../config/Functions.php';
+require '../../vendor/autoload.php';
+
+use Carbon\Carbon;
+
+$database = new Database();
+$functions = new Functions();
+$error = null;
+$data = null;
+$name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_STRING);
+$rating = filter_input(INPUT_POST, 'rating', FILTER_SANITIZE_STRING);
+$category = filter_input(INPUT_POST, 'category', FILTER_SANITIZE_STRING);
+$locations_id = filter_input(INPUT_POST, 'locations_id', FILTER_SANITIZE_STRING);
+$reputation = filter_input(INPUT_POST, 'reputation', FILTER_SANITIZE_STRING);
+//$reputationBadge = filter_input(INPUT_POST, 'reputationBadge', FILTER_SANITIZE_STRING);
+$price = filter_input(INPUT_POST, 'price', FILTER_SANITIZE_STRING);
+$availability = filter_input(INPUT_POST, 'availability', FILTER_SANITIZE_STRING);
+$reputationBadge = null;
+$file = $_FILES['image'];
+$pos = substr($file['name'], 0, strpos($file['name'], '.'));
+$filename = $pos . '_' . time();
+$ext = pathinfo($file['name'], PATHINFO_EXTENSION);
+if ($reputation <= 500) {
+    $reputationBadge = "red";
+} else if ($reputation > 500 && $reputation <= 799) {
+    $reputationBadge = "yellow";
+} else {
+    $reputationBadge = "green";
+}
+$now = Carbon::now()->format('Y-m-d H:i:s');
+$error = $functions->validateAccommodationCreate($locations_id, $name, $category, $rating, $reputation, $price, $availability, $file['size']);
+if (move_uploaded_file($file['tmp_name'], "../../images/$filename.$ext") && $error == null) {
+    $data = [
+        'name' => $name,
+        'rating' => $rating,
+        'category' => $category,
+        'locations_id' => $locations_id,
+        'image' => "localhost/php-test/images/$filename.$ext",
+        'reputation' => $reputation,
+        'reputationBadge' => $reputationBadge,
+        'price' => $price,
+        'availability' => $availability,
+        'created_at' => $now,
+        'updated_at' => $now
+    ];
+    $database->insert($data, $database->getPdo(), 'accommodations');
+}
+$foundLocation = $database->getvalues('locations','WHERE id = :id and is_deleted = 0',["id" => $locations_id]);
+$values = [
+    'name' => $name,
+    'rating' => $rating,
+    'category' => $category,
+    'location' => $foundLocation,
+    'image' => "localhost/php-test/images/$filename.$ext",
+    'reputation' => $reputation,
+    'reputationBadge' => $reputationBadge,
+    'price' => $price,
+    'availability' => $availability
+];
+// $values = [
+//     "city" => $city,
+//     "state" => $state,
+//     "country" => $country,
+//     "zip_code" => $zip_code,
+//     "address" => $address,
+//     "created_at" => $now,
+//     "updated_at" => $now
+// ];
+// $error = $functions->validateZipCode($zip_code, 'create');
+// if ($error == null) {
+//     $database->insert($values, $database->getPdo(), 'locations');
+// }
+$result = [
+    'data' => $error == null ? $values : null,
+    'error' => $error,
+    'message' => $error == null ? 'Successfully created' : 'An error occured'
+];
+echo json_encode($result);
+
+exit();
--- config/Functions.php
+++ config/Functions.php
@@ -1,6 +1,11 @@
 <?php
 class Functions{
 
+    protected $database;
+    public function __construct()
+    {
+        $this->database = new Database();
+    }
     public function validateZipCode($zip_code, $method = "create", $item = true)
     {
         $zip_code_length = strlen((string)$zip_code);
@@ -30,5 +35,50 @@ class Functions{
         }
         return $error;
 
+    }
+    public function validateAccommodationCreate($locations_id, $name, $category, $rating, $reputatation, $price, $availability, $file_size)
+    {
+
+        $found = $this->database->getvalues('locations','WHERE id = :id and is_deleted = 0',[":id" => $locations_id]);
+        $error = null;
+        if(!$found) {
+            $error = "The locations_id is not valid";
+        }
+        $category_array = ['hotel', 'alternative', 'hostel', 'lodge', 'resort', 'guest-house'];
+        if(!in_array($category, $category_array)) {
+            $error = "The category is not valid";
+        }
+        if(is_numeric($category)) {
+            $error = "The category should not be a number";
+        }
+        if(strlen($name) <= 10) {
+            $error = "The name should have more than 10 characters";
+        }
+        if(str_contains($name, 'Free') || str_contains($name, 'Offer') || str_contains($name, 'Book') || str_contains($name, 'Website')) {
+            $error = "The name should not contain the words(Free, Offer, Book, Website)";
+        }
+        if(!is_numeric($reputatation)){
+            $error = "The reputatation should be an integer";
+        }
+        if(!is_numeric($rating)){
+            $error = "The rating should be an integer";
+        }
+        if($rating < 0 && $rating > 5) {
+            $error = "The rating should be between 0 & 5";
+        }
+        if($reputatation < 0 || $reputatation > 1000) {
+            $error = "The reputation should be between 0 & 1000";
+        }
+        
+        if(!is_numeric($price)){
+            $error = "The price should be an integer";
+        }
+        if(!is_numeric($availability)){
+            $error = "The availability should be an integer";
+        }
+        if ($file_size > 2000000) {
+            $error = "File is too big";
+        }
+        return $error;
     }
 }
